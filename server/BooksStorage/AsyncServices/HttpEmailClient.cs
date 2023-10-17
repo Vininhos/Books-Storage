2455f37cff7048db750a5edf2d770dcf7ba5b840
Vininhos <viniciusrw2011@hotmail.com>
2023-10-16 23:27:44 -0300

    Finished Mailer Send Mail Function and adjusted appsettings.json

--- server/BooksStorage/AsyncServices/HttpEmailClient.cs
+++ server/BooksStorage/AsyncServices/HttpEmailClient.cs
@@ -1,12 +1,34 @@
+using System.Text;
+using System.Text.Json;
+
 namespace BooksStorage.AsyncServices;
 
 public class HttpEmailClient : IHttpEmailClient
 {
-    private HttpClient _client = new();
+    private readonly HttpClient _httpClient;
+    private readonly IConfiguration _configuration;
 
-    public Task SendMailRequest(Mail.Models.Mail mail)
+    public HttpEmailClient(HttpClient httpClient, IConfiguration configuration)
     {
-        //HttpResponseMessage response = await _client.PostAsJsonAsync("");
-        //TO DO
+        _httpClient = httpClient;
+        _configuration = configuration;
+    }
+
+    public async Task SendMailRequest(Mail.Models.Mail mail)
+    {
+        try
+        {
+            var httpContent = new StringContent(JsonSerializer.Serialize(mail), Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await _httpClient.PostAsync(_configuration["MailerService"], httpContent);
+
+            if (response.IsSuccessStatusCode)
+                Console.WriteLine("--> Sync POST to MailerService.");
+            else
+                Console.WriteLine("--> Sync POST to MailerService failed.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("--> Failed to send request to Mailer Service. Error: {0}", ex);
+        }
     }
 }
