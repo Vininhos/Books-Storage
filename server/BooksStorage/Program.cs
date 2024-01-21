e8fd28e3ea0b10995fe600feca700c3c7e8fe5ba
Isabel <isabeltosetto@gmail.com>
2024-01-21 17:29:20 -0300

    [feat] - configuração de log no serilog

--- server/BooksStorage/Program.cs
+++ server/BooksStorage/Program.cs
@@ -2,6 +2,8 @@ using BooksStorage.AsyncServices;
 using BooksStorage.Data;
 using BooksStorage.Models.Book;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +19,15 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(opt => { opt.UseInMemoryDatabase("InMem"); });
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+var elasticUrl = builder.Configuration.GetValue<string>("ElasticConfiguration:URL");
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(elasticUrl))
+    {
+        AutoRegisterTemplate = true
+    })
+    .CreateLogger();
 
 builder.Services.AddCors(options =>
 {
@@ -35,12 +46,15 @@ builder.Services.AddLogging(options =>
 });
 
 var app = builder.Build();
+var loggerFactory = app.Services.GetRequiredService<ILoggerFactory>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
   PrepDb.PrepPopulation(app);
 }
+
+loggerFactory.AddSerilog();
 
 app.UseSwagger();
 
