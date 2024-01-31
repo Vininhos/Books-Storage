6480a7eea94cbbbe68d715b755d6cc9d978ba11c
Vininhos <viniciusrw2011@hotmail.com>
2024-01-30 23:12:00 -0300

    Refactor: Refactored logger

--- server/mongodb-feeder/pkg/api/book.go
+++ server/mongodb-feeder/pkg/api/book.go
@@ -8,25 +8,20 @@ import (
 	"io"
 	"log/slog"
 	"math/rand"
+	"mongodb-feeder/pkg/logger"
 	"mongodb-feeder/pkg/model"
 	"net/http"
 	"os"
 )
 
-var logger *slog.Logger
-
-func init() {
-	logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
-}
-
 func GetNewBook(i int) (model.BooksStorageModel, error) {
-	logger.Info("Trying to get a new book from API.")
-	res, err := http.Get(fmt.Sprintf("https://stephen-king-api.onrender.com/api/book/%d", i))
+	logger.Logger.Info("Trying to get a new book from API.")
+	res, err := http.Get(fmt.Sprintf(os.Getenv("ANOTHERBOOKAPIURL")+"%d", i))
 	if err != nil {
 		return model.BooksStorageModel{}, err
 	}
 
-	logger.Info("Reading message from response body.")
+	logger.Logger.Info("Reading message from response body.")
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
 		return model.BooksStorageModel{}, err
@@ -36,37 +31,37 @@ func GetNewBook(i int) (model.BooksStorageModel, error) {
 		return model.BooksStorageModel{}, err
 	}
 
-	logger.Info("Response from API:", slog.String("response", string(body)))
+	logger.Logger.Info("Response from API:", slog.String("response", string(body)))
 
 	var book model.PublicApiBook
 
-	logger.Info("Executing JSON Unmarshal from response.")
+	logger.Logger.Info("Executing JSON Unmarshal from response.")
 	err = json.Unmarshal(body, &book)
 	if err != nil {
 		return model.BooksStorageModel{}, err
 	}
 
-	logger.Info("Returning new book.")
+	logger.Logger.Info("Returning new book.")
 	return model.BooksStorageModel{Name: book.Data.Title, Author: gofakeit.Name(), PublicationYear: book.Data.Year, Price: rand.Float64() * 3000, Category: "History", Email: gofakeit.Email()}, nil
 }
 
 func SendPayload(book model.BooksStorageModel) error {
-	logger.Info("Executing JSON Marshal from book:", slog.String("name", book.Name))
+	logger.Logger.Info("Executing JSON Marshal from book:", slog.String("name", book.Name))
 	body, err := json.Marshal(book)
 	if err != nil {
 		return err
 	}
 
-	logger.Info("Generating payload.")
+	logger.Logger.Info("Generating payload.")
 	payload := bytes.NewBuffer(body)
 
-	logger.Info("Posting payload to the API.")
-	res, err := http.Post("http://localhost:8079/api/Book", "application/json", payload)
+	logger.Logger.Info("Posting payload to the API.")
+	res, err := http.Post("BOOKSSTORAGEAPIURL", "application/json", payload)
 	if err != nil {
 		return err
 	}
 
-	logger.Info("Response Status Code:", slog.Int("statusCode", res.StatusCode))
+	logger.Logger.Info("Response Status Code:", slog.Int("statusCode", res.StatusCode))
 
 	return nil
 }
