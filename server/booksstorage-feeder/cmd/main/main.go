a21142817bdaf9cc62128eeae23f75185f33548f
Vininhos <viniciusrw2011@hotmail.com>
2024-03-24 22:44:33 -0300

    Refactor: Adjusted BooksStorage Feeder to read an environment variable to define the max random milliseconds to generate random payloads

--- server/booksstorage-feeder/cmd/main/main.go
+++ server/booksstorage-feeder/cmd/main/main.go
@@ -4,13 +4,27 @@ import (
 	"booksstorage-feeder/pkg/api"
 	"booksstorage-feeder/pkg/logger"
 	"booksstorage-feeder/pkg/model"
+	"fmt"
 	"log/slog"
 	"math/rand"
+	"os"
+	"strconv"
 	"time"
 )
 
 var book model.BooksStorageModel
 var err error
+var r int = 60000
+
+func init() {
+	rarg := os.Getenv("MAXMILLISEC")
+	if rarg != "" {
+		r, err = strconv.Atoi(rarg)
+		if err != nil {
+			panic(fmt.Sprintf("Error while converting environment variable to int. Error: %s", err))
+		}
+	}
+}
 
 func main() {
 	for {
@@ -21,7 +35,7 @@ func main() {
 			logger.Logger.Error("Error while sending a new book to the API:", slog.String("error", err.Error()))
 		}
 
-		r := rand.Intn(60000)
+		r := rand.Intn(r)
 
 		time.Sleep(time.Millisecond * time.Duration(r))
 	}
